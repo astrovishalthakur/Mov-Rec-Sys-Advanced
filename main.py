97e89eb83c13c7ca778d011b0741128e4b1d7ab3
astrovishalthakur <astrovishalthakur@gmail.com>
2022-11-04 14:39:52 +0530

    changed flask app port to 3000

--- main.py
+++ main.py
@@ -152,4 +152,4 @@ def recommend():
         movie_cards=movie_cards,reviews=movie_reviews,casts=casts,cast_details=cast_details)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
